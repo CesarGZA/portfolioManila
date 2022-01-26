3461c1ae452888e2fc52072309587315be4ae950
cesarGZA <cesarg_00@outlook.com>
2022-01-25 22:23:42 -0600

    dark-theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,4 +123,40 @@ modalCloses.forEach((modalC) => {
   }
   window.addEventListener('scroll',scrollActive)
 
+  /* ========================== CHANGE BACKGROUND HEADE =============== */
+  function scrollHeader(){
+        const nav = document.getElementById('header')
+        if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')    
+  }
+  window.addEventListener('scroll',scrollHeader)
 
+  /* ======================== SHOW SCROLL TOP ======================== */
+  function scrollUp(){
+      const scrollUp = document.getElementById('scroll-up');
+      if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  }
+  window.addEventListener('scroll',scrollUp)
+  /* ==================== DARK LIGHT THEME ===================== */
+  const themeButton = document.getElementById('theme-button')
+  const darkTheme = 'dark-theme'
+  const iconTheme = 'sun__icon'
+  
+  //Seleccionado previamente
+  const selectedTheme = localStorage.getItem('selected-theme')
+  const selectedIcon = localStorage.getItem('selected-icon')
+  //
+  const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+  const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'moon__icon' : 'sun__icon'
+  //
+  if(selectedTheme){
+      document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+      themeButton.classList[selectedIcon === 'moon__icon' ? 'add' : 'remove'](iconTheme)
+  }
+
+  //Active /desactive the theme manually whith the button
+  themeButton.addEventListener('click',() => {
+      document.body.classList.toggle(darkTheme)
+      themeButton.classList.toggle(iconTheme)
+      localStorage.setItem('selected-theme', getCurrentTheme())
+      localStorage.setItem('selected-icon', getCurrentIcon())
+  })
